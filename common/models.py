cc8a20e634ad95608e3ea81b19ee10e73b7abfea
mudit <mudit@mudit-ThinkPad-E420.(none)>
2013-08-15 09:47:58 +0530

    changes

---
+++ common/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.db import models
+from tinymce.models import HTMLField
+
+CONTACT_CHOICES = ((1,'Business Query'), (2,'Advertise with us'), (3,'Other Query'),)
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    text = HTMLField(max_length=1000)
+    mobile = models.CharField(max_length=12)
+    type = models.IntegerField(choices=CONTACT_CHOICES)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    moderation_date = models.DateTimeField(auto_now=True) 
+    status = models.IntegerField(choices=((0,'Submitted'),(1,'Viewed'),))
+    done_by = models.ForeignKey(User)
+
+class ContactUsText(models.Model):
+    text = HTMLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User)
+    
+class TermsNCondition(models.Model):
+    text = HTMLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User)
+     
+class AboutUs(models.Model):
+    text = HTMLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User)
+    
+class Enquiry(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    text = HTMLField(max_length=1000)
+    mobile = models.CharField(max_length=12)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    moderation_date = models.DateTimeField(auto_now=True) 
+    status = models.IntegerField(choices=((0,'Submitted'),(1,'Viewed'),))
+    done_by = models.ForeignKey(User)
