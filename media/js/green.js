00d159d1d293f3d87c1f56b33253dd9aa4bf55ed
mohit <mohit@mohit-ubuntu.(none)>
2013-08-23 12:12:08 +0530

    home page updated

--- media/js/green.js
+++ media/js/green.js
@@ -3,3 +3,12 @@ $('.cls_keyword').on('click',function(){
     $('#id_hdnkeyword').val($(this).attr('text'));
     $('#proxy_form').submit();
  });
+
+$('.cls_tab').on('click',function(){
+    var e = $(this);
+    e.h = $(this).attr('href').replace('#','');
+    $('.cls_homeform').hide();
+    $('.cls_tab_'+ e.h).show();
+    $('.cls_tab').removeClass('active');
+    $(this).addClass('active');
+});
