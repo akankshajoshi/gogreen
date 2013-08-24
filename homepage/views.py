796ac14e0dd806f322a60ac9d16bbce9bc80d2de
mudit <mudit@mudit-ThinkPad-E420.(none)>
2013-08-24 10:09:29 +0530

    changes

--- homepage/views.py
+++ homepage/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Heading, CompanyCount
-from website.models import Category, Subcategory, Company
+from website.models import Category, Subcategory, Company, Blog
 from django.db.models import Q
 
 def home(request):
+    comps = []
+    blogs = Blog.objects.all().order_by('-creation_date')[:5]
     if request.method == 'GET':
         heading = Heading.objects.all()[0]
         count = CompanyCount.objects.all()[0]
-        blog = Blog.objects.all().order_by('-creation_date')[:5]
-        return render_to_response('home/home.html',{'heading':heading, 'count':count,'blogs':blogs}, context_instance=RequestContext(request))
+        return render_to_response('home/home.html',{'heading':heading, 'count':count, 'blogs':blogs}, context_instance=RequestContext(request))
     else:
-        search_by = request.POST.get('field')
+        cats = Category.objects.all()
         keyword = request.POST.get('keyword')
-        if search_by == 'company':
-            results = Company.objects.filter(company_name__icontains=keyword)
-        else:
-            cats = Category.objects.filter(name__icontains=keyword)
-            subcats = Subcategory.objects.filter(Q(name__icontains=keyword) | Q(category__in=cats))
-            results = Company.objects.filter(Q(subcategory__in=subcats) | Q(business_description__icontains=keyword)).distinct()
-        return render_to_response('search/comp_search.html', {'results':results}, context_instance=RequestContext(request))
+        search_by = request.POST.get('search_by')
+        if keyword:
+            if search_by == 'by_name':
+                comps = Company.objects.filter(company_name__icontains=keyword)
+            else:
+                comps = Company.objects.filter(deals_in__icontains=keyword)
+        return render_to_response('search/comp_search.html', {'cats': cats, 'comps':comps}, context_instance=RequestContext(request))
 
     
