3c9a837ffa200bc3251ff1e7b061f5c1045ca8b5
mudit <mudit@mudit-ThinkPad-E420.(none)>
2013-08-27 00:04:45 +0530

    some cahnges upto cpompany page

--- homepage/views.py
+++ homepage/views.py
@@ -8,25 +8,11 @@ from datetime import datetime as dt
 
 def home(request):
     comps = []
-
     blogs = Blog.objects.all().order_by('-creation_date')[:5]
-    govn_logos = GovrnLogo.objects.all()
     homelogos = HomePageLogo.objects.filter(start_date__lte=dt.now().date(), end_date__gte=dt.now().date())
     if request.method == 'GET':
-
         count = CompanyCount.objects.all()[0]
-
         return render_to_response('home/home.html',
-                                  {'count':count, 'blogs':blogs,'govn_logos':govn_logos,'homelogos':homelogos},
+                                  {'count':count, 'blogs':blogs,'homelogos':homelogos},
                                   context_instance=RequestContext(request))
-    else:
-        keyword = request.POST.get('keyword')
-        search_by = request.POST.get('search_by')
-        if keyword:
-            if search_by == 'by_name':
-                comps = Company.objects.filter(company_name__icontains=keyword)
-            else:
-                comps = Company.objects.filter(deals_in__icontains=keyword)
-        return render_to_response('search/comp_search.html', {'comps':comps}, context_instance=RequestContext(request))
-
     
